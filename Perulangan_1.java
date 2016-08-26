1b6562232c2b9bca725768120344aa469be77861
Anisa Reviyana Sakti <anisareviyana@gmail.com>
2016-08-26 13:25:29 +0700

    Add files via upload

---
+++ Perulangan_1.java
@@ -0,0 +1,17 @@
+package belajar_java;
+
+public class Perulangan_1 {
+public static void main(BelajarString[]args){
+	int nilai = 10;
+	while(nilai>=1){
+		System.out.println(nilai);
+		nilai--;
+	}
+	System.out.println("----------------------------");
+	int nilai2 = 1;
+	do{
+		System.out.println(nilai2);
+		nilai2++;
+	}while(nilai2<=10);
+}
+}
